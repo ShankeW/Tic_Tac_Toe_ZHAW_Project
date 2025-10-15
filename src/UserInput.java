3b6d8972b1cbf05e343bed2adf7a7c0cb18e6c21
Ahalya Sridharan <sridhaha@students.zhaw.ch>
2025-10-15 15:03:55 +0200

    Code in der Klasse UserInput geschrieben.

--- src/UserInput.java
+++ src/UserInput.java
@@ -1,2 +1,13 @@
 public class UserInput {
+    public UserInput() {
+
+    }
+
+    public String getInput() {
+        return "";
+    }
+
+    private int checkInput (String input) {
+        return 0;
+    }
 }
