870862f97b8d242c296de6b71896941b1306a9f3
Hunter Wilson <hunterinswitzerland@gmail.com>
2025-10-15 14:45:09 +0200

    Added Skeleton Code to TicTacToe and LanguageController.

--- src/LanguageController.java
+++ src/LanguageController.java
@@ -1,2 +1,19 @@
 public class LanguageController {
+    int LanguageSetting;
+    String[] EnglishMessages;
+    String[] GermanMessages;
+
+    public LanguageController() {
+
+    }
+    //Sets the Languge Setting with an Int
+    public void setLanguageSetting(int languageSetting) {
+        this.LanguageSetting = languageSetting;
+    }
+
+    //Gets a Message in the Set Language by the given Int
+    public String getMessage(int messageIndex){
+
+        return "";
+    }
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,2 +1,23 @@
 public class TicTacToe {
+    Board board;
+    InputController inputController;
+    LanguageController languageController;
+
+    public TicTacToe(){
+        this.board = new Board();
+        this.inputController = new InputController();
+        this.languageController = new LanguageController();
+    }
+
+    private void playTurn(){
+
+    }
+
+    private void sendMessage(){
+
+    }
+
+    public void restartGame(){
+
+    }
 }
