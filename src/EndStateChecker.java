4c849071a6fc803ead76323c842d629249d7eabb
Shanke Wei <weisha01@students.zhaw.ch>
2025-10-15 14:58:19 +0200

    skelett EndStateChecker

--- src/EndStateChecker.java
+++ src/EndStateChecker.java
@@ -1,2 +1,14 @@
 public class EndStateChecker {
+    /*
+     * Initialisiere die Objekt dieser Klasse
+     */
+    public EndStateChecker(){}
+
+    /*
+     * Die Methode kontrolliert den Status des Spielbretts.
+     */
+    public int CheckForEndState(int[] FieldState){
+        // TODO
+        return -1;
+    }
 }
