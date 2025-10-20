dd2b9e04ef8124e56aa23212977baf14674fd741
Shanke Wei <weisha01@students.zhaw.ch>
2025-10-20 14:22:53 +0200

    Dokumentation für die Klasse ValidMoveChecker wird vervollständigt.

--- src/ValidMoveChecker.java
+++ src/ValidMoveChecker.java
@@ -1,9 +1,23 @@
+/**
+ * Diese Klasse kontrolliert, ob den aktuell gemachten Spielzug gültig ist,
+ * bzw ob der Spielstein auf der gewünschten Position gesetzt werden darf.
+ */
 public class ValidMoveChecker {
 
-    // Constructor
+    /**
+     * Der Instanz dieser Klasse wird erzeugt.
+     */
     public ValidMoveChecker() {
     }
 
+    /**
+     * Diese Methode
+     * @param FieldStates repräsentiert das Board.
+     * @param Field soll die Position, bzw das Feld, wo man den Spielstein setzen möchtet, darstellen.
+     * @return gibt an, ob ein solcher Zug:
+     *    true: gemacht werden kann,
+     *    false: nicht gemacht werden kann.
+     */
     public boolean Check(int[] FieldStates, int Field) {
         // TODO
         if (Field < 1 || Field > 9){
