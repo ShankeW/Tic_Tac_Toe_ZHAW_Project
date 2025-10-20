bfa26c15e6f69a366260d80e8c08e5a7316366ef
Hunter Wilson <hunterinswitzerland@gmail.com>
2025-10-20 14:23:08 +0200

    Created TestBoard

---
+++ test/TestBoard.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestBoard {
+    @Test
+    public void testBoardRestart(){
+        Board board = new Board();
+        board.setFieldState(1,1);
+        board.setFieldState(4,2);
+        board.setFieldState(2,1);
+        board.setFieldState(5,2);
+        board.setFieldState(3,1);
+
+        assertEquals(1,board.EndState);
+
+        board.resetBoard();
+
+        assertEquals(0, board.EndState);
+    }
+}
